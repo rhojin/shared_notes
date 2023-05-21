024a5c25886ca67ef6d9946b654ae1e9ad09c835
rhojin <130408753+rhojin@users.noreply.github.com>
2023-05-21 19:48:16 +0200

    feat: swipe gesture to delete note | edit button on note

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,17 @@ import 'package:shared_notes/grpc/Notes.pbgrpc.dart';
 
 class NoteServiceClientProvider extends ChangeNotifier {
   NoteServiceClient noteServiceClient;
-  List<String> items = []; //TODO: extract to as dedicated ChangeNotifier
+  List<Note> items = []; //TODO: extract to as dedicated ChangeNotifier
   TextEditingController createNoteTextController = TextEditingController();
+  TextEditingController updateNoteTextController = TextEditingController();
 
   NoteServiceClientProvider(this.noteServiceClient);
 
   void refresh() async {
     try {
       final response = await noteServiceClient.getNotes(Empty());
-      items = response.note.map((note) => note.text).toList();
+      items = response.note;
+      items.sort((a, b) => a.text.compareTo(b.text));
     } catch (e) {
       print('Caught error: $e');
     }
@@ -49,8 +51,9 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: title,
       home: ChangeNotifierProvider(
-        create: (context) => NoteServiceClientProvider(stub),
+        create: (context) => NoteServiceClientProvider(stub)..refresh(),
         child: Scaffold(
+            backgroundColor: Colors.white,
             appBar: AppBar(
                 title: const Text(title),
                 actions: [
@@ -74,7 +77,10 @@ class App extends StatelessWidget {
                   ),
                 ],
             ),
-            body: const Notes(),
+            body: Container(
+                padding: const EdgeInsets.all(15),
+                child: const Notes(),
+            ),
         ),
       )
     );
@@ -125,8 +131,11 @@ class CreateButton extends StatelessWidget {
                             String text = notifier.createNoteTextController.text;
                             if(text.isNotEmpty) {
                               var note = Note(text: text);
-                              notifier.noteServiceClient.createNote(note);
-                              notifier.refresh();
+                              notifier.noteServiceClient.createNote(note)
+                                  .then((note) {
+                                    notifier.refresh();
+                                    notifier.createNoteTextController.text = '';
+                              });
                             }
                             Navigator.pop(context);
                           },
@@ -150,17 +159,93 @@ class Notes extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<NoteServiceClientProvider> (
       builder: (context, notifier, child) {
-        return ListView.builder(
+        return ListView.separated(
             itemCount: notifier.items.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: const Color(0xff1f92fc),
-                  child: Text(style: const TextStyle(color: Color(0xffffffff)), notifier.items[index][0]),
-                ),
-                title: Text(notifier.items[index]),
+              final item = notifier.items[index];
+              return Dismissible(
+                  key: Key(item.id.toString()),
+                  onDismissed: (direction) {
+                    NoteId noteId = NoteId.create()
+                      ..id = item.id;
+                    notifier.noteServiceClient.deleteNote(noteId)
+                        .then((note) {
+                          notifier.refresh();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('${note.text} removed')));
+                      });
+                  },
+                  direction: DismissDirection.endToStart,
+                  background: Container(color: Colors.red),
+                  child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: const Color(0xff1f92fc),
+                        child: Text(style: const TextStyle(color: Color(0xffffffff)), notifier.items[index].text[0]),
+                      ),
+                      title: Text(notifier.items[index].text),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.blue,
+                        ),
+                        onPressed: () {
+                          notifier.updateNoteTextController.text = item.text;
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                            return Dialog(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                              elevation: 16,
+                              child: Container(
+                                height: 50,
+                                width: 100,
+                                color: Colors.white,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    SizedBox(
+                                      height: 40,
+                                      width: 200,
+                                      child: TextField(
+                                        controller: notifier.updateNoteTextController,
+                                        textAlignVertical: const TextAlignVertical(y: 1),
+                                        textAlign: TextAlign.left,
+                                        decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          hintText: 'Enter note text',
+                                        ),
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        String text = notifier.updateNoteTextController.text;
+                                        if(text.isNotEmpty) {
+                                          item.text = text;
+                                          notifier.noteServiceClient.updateNote(item).then((item) {
+                                            notifier.refresh();
+                                            notifier.updateNoteTextController.text = '';
+                                          });
+                                        }
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("Submit"),
+                                    ),
+                                  ],
+                                  ),
+                                )
+                              );
+                            });
+                          },
+                        ),
+                    ),
               );
-            }
+          },
+          separatorBuilder: (context, index) {
+            return const Divider(
+              color: Colors.blue,
+              thickness: 2,
+            );
+          },
         );
       }
     );
